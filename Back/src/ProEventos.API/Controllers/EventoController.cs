959f0d13aa4716e3bb7e12554f4759956e8ffbe1
Washington <washin.elvira@gmail.com>
2023-11-29 15:13:29 -0300

    Seção 3: Introdução ao EF Core

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
@@ -13,44 +14,23 @@ namespace ProEventos.API.Controllers
     public class EventoController : ControllerBase
     {
 
-        public IEnumerable<Evento> _evento = new Evento[]{
+        private readonly DataContext _context;
 
-            new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-            },
-            new Evento(){
-                EventoId = 2,
-                Tema = "Angular 11 e .NET 5 e suas novidades",
-                Local = "Sao Paulo",
-                Lote = "2º Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-            }
-
-        };
-
-        public EventoController()
+        public EventoController(DataContext context)
         {
-
+            this._context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento;            
+            return _context.Eventos;            
         }
 
         [HttpGet("{id}")]
         public IEnumerable<Evento> Get(int id)
         {
-            return _evento.Where(evento => evento.EventoId==id);
+            return _context.Eventos.Where(evento => evento.EventoId==id);
         }
 
     }
